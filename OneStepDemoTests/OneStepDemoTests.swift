9f7abfc08ca9b2890540b2480997ab120f45fddb
Amjad Khalil <akhalil@getreceet.com>
2020-11-22 10:01:08 +0200

    Initial Commit

---
+++ OneStepDemoTests/OneStepDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  OneStepDemoTests.swift
+//  OneStepDemoTests
+//
+//  Created by Amjad on 11/22/20.
+//
+
+import XCTest
+@testable import OneStepDemo
+
+class OneStepDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
